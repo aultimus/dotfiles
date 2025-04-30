f5826e034af78e142ce71f743db8b8defd27bc76
Matthew Ault <matthew.ault@gmail.com>
2025-04-30 09:18:44 +0100

    add direnv install to install.sh

--- install.sh
+++ install.sh
@@ -10,9 +10,15 @@ BASE=$( dirname -- "$( readlink -f -- "$0"; )"; )""
 
 ln -sf $BASE/bash/.bashrc ~/.bashrc
 
-# install go version manager
+# --- install go version manager
 curl -sL -o ~/bin/gvm https://github.com/andrewkroh/gvm/releases/download/v0.5.2/gvm-linux-amd64
 chmod +x ~/bin/gvm
+# --- install go version manager
+
+# --- install direnv
+sudo apt-get install direnv
+# --- install direnv
+
 
 . ~/.bashrc
 
